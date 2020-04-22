4a5bada041580d33bbca0be174e742c1a6e9bc31
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-22 11:27:37 -0700

    add elements to program from API call.

--- src/cocktail.js
+++ src/cocktail.js
@@ -17,4 +17,3 @@ export class Cocktail {
     }
   }
 }
-//let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from "jquery";
+import $ from "jquery";
 import { Cocktail } from './../src/cocktail.js';
 
 // $(document).ready(function () {
-  // $("#rondomCocktail").click(function () {
-    
-  // });
+// $("#rondomCocktail").click(function () {
+//   let cocktail = new Cocktail;
+//   cocktail.randomCocktail();
+//   // });
+//   let getElements = function (jsonifiedResponse) {
+//     $('#cocktailReturn').html(`${jsonifiedResponse.drinks[0].strDrink}`);
+//   getElements();
+//   };
 // });
-let cocktail = new Cocktail;
-cocktail.randomCocktail();
+
+$(document).ready(function() {
+
+  $('#randomCocktail').click(function() {
+    $('#cocktailReturn').html("")
+    (async () => {
+      let cocktail = new Cocktail();
+      const response = await cocktail.randomCocktail();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $('#cocktailName').html(`${response.drinks[0].strDrink}`);
+        $('#cocktailReturn').append("<li>" + `${response.drinks[0].strMeasure1}` + " " + `${response.drinks[0].strIngredient1}` + "</li><li>this is another test</li>");
+      } else {
+        $('#cocktailReturn').text(`There was an error handling your request.`);
+      }
+    }
+
+  });
+});
